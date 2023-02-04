ec4fd481632e37c0b42b52ae08ff9a49613b3f9e
michaelsatumba <mjsatumba@gmail.com>
2023-02-04 10:05:49 -0800

    working fetch api

--- app.js
+++ app.js
@@ -4,85 +4,141 @@ const path = require('path');
 require('dotenv').config();
 const readline = require('readline-sync');
 const axios = require('axios');
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
-const server = http.createServer(function (request, response) {
-	console.log('request starting...');
+const auth = Buffer.from(client_id + ':' + client_secret).toString('base64');
 
-	let filePath = './public';
-	let contentType = 'text/html';
-
-	if (request.url.endsWith('.css')) {
-		filePath += request.url;
-		contentType = 'text/css';
-	} else if (request.url.endsWith('.js')) {
-		filePath += request.url;
-		contentType = 'text/javascript';
-	} else {
-		filePath += '/index.html';
-	}
-
-	fs.readFile(filePath, function (error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-		} else {
-			response.writeHead(200, { 'Content-Type': contentType });
-			response.end(content, 'utf-8');
-		}
-	});
-});
-
-server.listen(3000);
-console.log('Server running at http://localhost:3000/');
-
-var client_id = process.env.CLIENT_ID;
-var client_secret = process.env.CLIENT_SECRET;
-
-var auth = Buffer.from(client_id + ':' + client_secret).toString('base64');
-
-var config = {
+const config = {
 	headers: {
 		Authorization: 'Basic ' + auth,
 	},
 };
 
-function logNBAGamesInCity() {
-	var city = readline.question('Enter the city name (or exit to quit): ');
-
-	if (city === 'exit') {
-		console.log('Goodbye!');
-		return;
-	}
-
-	axios
-		.get(
+const server = http.createServer(function (request, response) {
+	if (request.url.startsWith('/search')) {
+		const city = request.url.split('=')[1];
+		fetch(
 			'https://api.seatgeek.com/2/events?taxonomies.name=nba&venue.city=' +
 				city,
 			config
 		)
-		.then(function (response) {
-			console.log('NBA games in ' + city + ':');
-			var events = response.data.events;
-			events.forEach(function (event) {
-				var date = new Date(event.datetime_local);
-				console.log(
-					'- ' +
-						event.title +
-						' at ' +
-						event.venue.name +
-						' on ' +
-						date.toLocaleDateString() +
-						'\nLink to purchase tickets: ' +
-						event.url
-				);
+			.then(function (seatGeekResponse) {
+				return seatGeekResponse.json();
+			})
+			.then(function (seatGeekData) {
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify(seatGeekData));
+			})
+			.catch(function (error) {
+				response.writeHead(500, { 'Content-Type': 'text/plain' });
+				response.end('Error');
 			});
+	} else {
+		let filePath = './public';
+		let contentType = 'text/html';
 
-			logNBAGamesInCity();
-		})
-		.catch(function (error) {
-			console.error(error);
-			logNBAGamesInCity();
+		if (request.url.endsWith('.css')) {
+			filePath += request.url;
+			contentType = 'text/css';
+		} else if (request.url.endsWith('.js')) {
+			filePath += request.url;
+			contentType = 'text/javascript';
+		} else {
+			filePath += '/index.html';
+		}
+
+		fs.readFile(filePath, function (error, content) {
+			if (error) {
+				response.writeHead(500);
+				response.end();
+			} else {
+				response.writeHead(200, { 'Content-Type': contentType });
+				response.end(content, 'utf-8');
+			}
 		});
-}
+	}
+});
+server.listen(3000);
+console.log('Server running at http://localhost:3000/');
+// const server = http.createServer(function (request, response) {
+// 	console.log('request starting...');
 
-logNBAGamesInCity();
+// 	let filePath = './public';
+// 	let contentType = 'text/html';
+
+// 	if (request.url.endsWith('.css')) {
+// 		filePath += request.url;
+// 		contentType = 'text/css';
+// 	} else if (request.url.endsWith('.js')) {
+// 		filePath += request.url;
+// 		contentType = 'text/javascript';
+// 	} else {
+// 		filePath += '/index.html';
+// 	}
+
+// 	fs.readFile(filePath, function (error, content) {
+// 		if (error) {
+// 			response.writeHead(500);
+// 			response.end();
+// 		} else {
+// 			response.writeHead(200, { 'Content-Type': contentType });
+// 			response.end(content, 'utf-8');
+// 		}
+// 	});
+// });
+
+// server.listen(3000);
+// console.log('Server running at http://localhost:3000/');
+
+// const client_id = process.env.CLIENT_ID;
+// const client_secret = process.env.CLIENT_SECRET;
+
+// const auth = Buffer.from(client_id + ':' + client_secret).toString('base64');
+
+// const config = {
+// 	headers: {
+// 		Authorization: 'Basic ' + auth,
+// 	},
+// };
+
+// function logNBAGamesInCity() {
+// 	const city = readline.question('Enter the city name (or exit to quit): ');
+
+// 	if (city === 'exit') {
+// 		console.log('Goodbye!');
+// 		return;
+// 	}
+
+// 	axios
+// 		.get(
+// 			'https://api.seatgeek.com/2/events?taxonomies.name=nba&venue.city=' +
+// 				city,
+// 			config
+// 		)
+// 		.then(function (response) {
+// 			console.log('NBA games in ' + city + ':');
+// 			const events = response.data.events;
+// 			events.forEach(function (event) {
+// 				const date = new Date(event.datetime_local);
+// 				console.log(
+// 					'- ' +
+// 						event.title +
+// 						' at ' +
+// 						event.venue.name +
+// 						' on ' +
+// 						date.toLocaleDateString() +
+// 						'\nLink to purchase tickets: ' +
+// 						event.url
+// 				);
+// 			});
+
+// 			logNBAGamesInCity();
+// 		})
+// 		.catch(function (error) {
+// 			console.error(error);
+// 			logNBAGamesInCity();
+// 		});
+// }
+
+// logNBAGamesInCity();
--- public/script.js
+++ public/script.js
@@ -1,1 +1,34 @@
-alert('Hello');
+document
+	.getElementById('searchForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		const city = document.getElementById('city').value;
+		const url = 'http://localhost:3000/search?city=' + city;
+
+		fetch(url)
+			.then(function (response) {
+				// console.log(response);
+				return response.json();
+			})
+			.then(function (data) {
+				console.log(data);
+				const events = data.events;
+				let html = '';
+				events.forEach(function (event) {
+					const date = new Date(event.datetime_local);
+					html +=
+						'<p>' +
+						event.title +
+						' at ' +
+						event.venue.name +
+						' on ' +
+						date.toLocaleDateString() +
+						'</p>';
+				});
+				document.getElementById('results').innerHTML = html;
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	});
